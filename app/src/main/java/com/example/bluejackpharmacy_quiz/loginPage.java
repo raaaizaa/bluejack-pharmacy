1ac51d38340402fc359b8e25e60e30e7d4c857c3
raaizar <rahmanraiza@gmail.com>
2023-03-21 16:17:57 +0700

    masih error di registerpage buat nyimpen datanya

--- app/src/main/java/com/example/bluejackpharmacy_quiz/loginPage.java
+++ app/src/main/java/com/example/bluejackpharmacy_quiz/loginPage.java
@@ -2,13 +2,33 @@ package com.example.bluejackpharmacy_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginPage extends AppCompatActivity {
+    private Button goToRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        goToRegister = (Button) findViewById(R.id.goToRegisterButton);
+
+
+
+        goToRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openRegisterPage();
+            }
+        });
+    }
+
+    public void openRegisterPage(){
+        Intent intent = new Intent(this, registerPage.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/bluejackpharmacy_quiz/registerPage.java
+++ app/src/main/java/com/example/bluejackpharmacy_quiz/registerPage.java
@@ -2,13 +2,99 @@ package com.example.bluejackpharmacy_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class registerPage extends AppCompatActivity {
+    private Button goToLogin, register;
+    private EditText name, email, pass, confirmPass, phoneNumber;
+    Integer id = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        name = findViewById(R.id.registerNameField);
+        email = findViewById(R.id.registerEmailField);
+        pass = findViewById(R.id.registerPasswordField);
+        confirmPass = findViewById(R.id.registerConfirmPasswordField);
+        phoneNumber = findViewById(R.id.registerPhoneField);
+        goToLogin = findViewById(R.id.goToLoginButton);
+        register = findViewById(R.id.registerButton);
+
+        goToLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openLoginPage();
+            }
+        });
+
+        register.setOnClickListener(e -> {
+            String inputtedName = name.getText().toString();
+            String inputtedEmail = email.getText().toString();
+            String inputtedPass = pass.getText().toString();
+            String inputtedConfirmPass = confirmPass.getText().toString();
+            String inputtedPhone = phoneNumber.getText().toString();
+
+
+            if(inputtedName.equals("") || inputtedEmail.equals("") || inputtedPass.equals("") || inputtedConfirmPass.equals("") || inputtedPhone.equals("")){
+                Toast.makeText(this, "All fields must be filled!", Toast.LENGTH_SHORT).show();
+            }else if(inputtedName.length() != 5){
+                Toast.makeText(this, "Name length must be at least 5 characters!", Toast.LENGTH_SHORT).show();
+            }else if(!Patterns.EMAIL_ADDRESS.matcher(inputtedEmail).matches()){
+                Toast.makeText(this, "Enter a valid email address!", Toast.LENGTH_SHORT).show();
+            }else if(!alphanumericValidator(inputtedPass)){
+                Toast.makeText(this, "Password must be alphanumeric!", Toast.LENGTH_SHORT).show();
+            }else if(!inputtedPass.equals(inputtedConfirmPass)){
+                Toast.makeText(this, "Password doesn't match!", Toast.LENGTH_SHORT).show();
+            }else if(!PhoneNumberUtils.isGlobalPhoneNumber(inputtedPhone)){
+                Toast.makeText(this, "Your phone number is not a valid phone number!", Toast.LENGTH_SHORT).show();
+            }else{
+                id++;
+                saveToTextFile(id, inputtedName, inputtedEmail, inputtedPass, inputtedPhone);
+            }
+
+        });
+    }
+
+    public void openLoginPage(){
+        Intent intent = new Intent(this, loginPage.class);
+        startActivity(intent);
+    }
+
+    public static boolean alphanumericValidator(String string){
+        String regex = "^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]+$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(string);
+
+        return m.matches();
+    }
+
+    public void saveToTextFile(Integer id, String name, String email, String password, String phoneNumber){
+
+        try {
+            FileWriter Writer = new FileWriter("user-information.txt", true);
+            BufferedWriter Buffered = new BufferedWriter(Writer);
+
+            Buffered.write(id + name + email + password + phoneNumber);
+            Buffered.newLine();
+            Buffered.close();
+            Writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
